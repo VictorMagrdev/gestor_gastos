cb3ec09afafdeb097e81029ee924fc86f0754eca
juanf <yioyiomain@gmail.com>
2024-04-01 16:53:51 -0500

    clase egresos

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import Any, Optional, List
 
+from datetime import datetime, time, timedelta
+from typing import Annotated
+from uuid import UUID
 
 app = FastAPI()
 
@@ -11,3 +14,10 @@ app = FastAPI()
 @app.get("/")
 def great():
     return {"Hello": "World"}
+
+class Egreso(BaseModel):
+    id: int | None = Field(default=None, primary_key=True)
+    fecha: Annotated[datetime | None, Body()] = None
+    descripcion: str = Field(min_length=4, max_length=64, title="email of the user")
+    valor: float = Field(default="0", le=5000000, lg=100, title="Price of the product")
+    categoria: str = Field(min_length=4, max_length=128, title="password of the user")
