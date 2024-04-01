72c3f05f62125ea7d7614d6844a956961a4061f1
Victor Manuel <victorakiladev@gmail.com>
2024-04-01 17:34:44 -0500

    feature:endpoinds implementation

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from datetime import datetime, time, timedelta
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field, validator
+from datetime import datetime
 from fastapi.responses import JSONResponse
-
-from pydantic import BaseModel, Field
-from typing import Any, Optional, List
-
-from datetime import datetime, time, timedelta
-from typing import Annotated
-from uuid import UUID
 
 app = FastAPI()
 
+contador = 0
+egresos = []
+ingresos = []
 
+ingresos_categorias = ["Pago de nomina", "Pago contrato", "Pago arriendo", "Mesada"]
+egreso_categorias= ["alimentacion", "transporte", "ocio", "libros"]
 @app.get("/")
 def great():
     return {"Hello": "World"}
@@ -24,10 +23,105 @@ class Egreso(BaseModel):
     valor:              float = Field(default="1000", le=5000000, lg=100, title="Price of entry transaction")
     categoria:          str = Field(min_length=4, max_length=128, title="category of entry transaction")
     
+    @validator("categoria")
+    @classmethod
+    def validar_categoria_egreso(cls, categoria):
+        if categoria not in egreso_categorias:
+            raise ValueError("Categoria incorrecta para los egresos")
+        return categoria
+    
 class Ingreso(BaseModel):
     id:                 int | None = Field(default=None, primary_key=True)
     fecha:              Annotated[datetime | None, Body()] = Field(title="entry transaction date")
     descripcion:        str = Field(min_length=4, max_length=64, title="entry transaction description")
     valor:              float = Field(default="1000", le=5000000, lg=100, title="Price of entry transaction")
     categoria:          str = Field(min_length=4, max_length=128, title="category of entry transaction")
+
+    @validator("categoria")
+    @classmethod
+    def validar_categoria_ingreso(cls, categoria):
+        if categoria not in ingresos_categorias:
+            raise ValueError("Categoria incorrecta para los ingresos")
+        return categoria
+        
+#ENDPOINTS INGRESOS
+@app.get('/ingresos', tags=['ingresos'], response_model=List[Ingreso], description="Returns all ingresos stored")
+def get_all_ingresos() -> List[Ingreso]:
+    result = []
+    for element in ingresos:
+        result.append(element)
+    return JSONResponse(content=result, status_code=200)
+
+@app.get('/ingresos/{id}', tags=['ingresos'], response_model=Ingreso, description="Returns data of one specific ingreso")
+def get_ingreso(id: int) -> Ingreso:
+    for element in ingresos:
+        if element["id"] == id:
+            return JSONResponse(content=element, status_code=200)
+    return JSONResponse(content=None, status_code=404)
+
+@app.post('/ingresos', tags=['ingresos'], response_model=dict, description="Creates a new ingreso")
+def create_ingreso(ingreso: Ingreso = Body()) -> dict:
+    global contador
+    ingreso.id = contador
+    contador += 1
+    ingresos.append(ingreso.model_dump())
+    return JSONResponse(content={
+        "message": "The ingreso was created successfully",
+        "data": ingreso.model_dump()
+    }, status_code=201)
+
+@app.delete('/ingresos/{id}', tags=['ingresos'], response_model=dict, description="Removes specific ingreso")
+def remove_user(id: int) -> dict:
+    for element in ingresos:
+        if element['id'] == id:
+            ingresos.remove(element)
+            return JSONResponse(content={
+                "message": "The ingreso was removed successfully",
+                "data": None
+            }, status_code=204)
+    return JSONResponse(content={
+        "message": "The ingreso does not exists",
+        "data": None
+    }, status_code=404)
+    
+#egresos
+@app.get('/egresos', tags=['egresos'], response_model=egresos[Egreso], description="Returns all egresos stored")
+def get_all_egresos() -> List[Egreso]:
+    result = []
+    for element in egresos:
+        result.append(element)
+    return JSONResponse(content=result, status_code=200)
+
+@app.get('/egresos/{id}', tags=['egresos'], response_model=Egreso, description="Returns data of one specific egreso")
+def get_egreso(id: int) -> Egreso:
+    for element in egresos:
+        if element["id"] == id:
+            return JSONResponse(content=element, status_code=200)
+    return JSONResponse(content=None, status_code=404)
+
+@app.post('/egresos', tags=['egresos'], response_model=dict, description="Creates a new egreso")
+def create_egreso(egreso: Egreso = Body()) -> dict:
+    global contador
+    egresos.id = contador
+    contador += 1
+    egresos.append(egreso.model_dump())
+    return JSONResponse(content={
+        "message": "The user was created successfully",
+        "data": egresos.model_dump()
+    }, status_code=201)
+
+@app.delete('/egresos/{id}', tags=['egresos'], response_model=dict, description="Removes specific egreso")
+def remove_egreso(id: int) -> dict:
+    for element in egresos:
+        if element['id'] == id:
+            egresos.remove(element)
+            return JSONResponse(content={
+                "message": "The egreso was removed successfully",
+                "data": None
+            }, status_code=204)
+    return JSONResponse(content={
+        "message": "The egreso does not exists",
+        "data": None
+    }, status_code=404)
+
 
