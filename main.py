07e7e9fe11bea790b9dab56711898b46a2edc639
Victor Manuel <victorakiladev@gmail.com>
2024-04-01 17:49:35 -0500

    datetime

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Annotated, List
+from typing import Annotated, List, Optional
 from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field, validator
 from datetime import datetime
@@ -32,7 +32,7 @@ class Egreso(BaseModel):
     
 class Ingreso(BaseModel):
     id:                 int | None = Field(default=None, primary_key=True)
-    fecha:              Annotated[datetime | None, Body()] = Field(title="entry transaction date")
+    fecha:              datetime | None = Field(default=None, title="Entry transaction date")
     descripcion:        str = Field(min_length=4, max_length=64, title="entry transaction description")
     valor:              float = Field(default="1000", le=5000000, lg=100, title="Price of entry transaction")
     categoria:          str = Field(min_length=4, max_length=128, title="category of entry transaction")
@@ -63,6 +63,7 @@ def get_ingreso(id: int) -> Ingreso:
 def create_ingreso(ingreso: Ingreso = Body()) -> dict:
     global contador
     ingreso.id = contador
+    ingreso.fecha = datetime.now()
     contador += 1
     ingresos.append(ingreso.model_dump())
     return JSONResponse(content={
