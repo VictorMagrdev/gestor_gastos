cf0942aed8e2023f654fa6bd94e9088fb86381f9
juanf <yioyiomain@gmail.com>
2024-04-14 22:35:16 -0500

    Avance guia persistencia

--- src/routers/ingresos.py
+++ src/routers/ingresos.py
@@ -1,41 +1,59 @@
 from datetime import datetime
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, status, Query, Path
 from fastapi.responses import JSONResponse
 from typing import List
 from src.schemas.ingreso import Ingreso
 from src.routers.contador import contador
 from src.config.database import SessionLocal
-from src.models.product import Product as ProductModel
+from src.models.ingreso import Ingreso as IngresoModel
 from fastapi.encoders import jsonable_encoder
 
 ingreso_router = APIRouter()
 
+@ingreso_router.get('/',
+                    tags=['ingresos'],
+                    response_model=List[Ingreso],
+                    description="Returns all ingresos stored")
+def get_all_ingresos(offset: int = Query(default=None, min=0),
+                    limit: int = Query(default=None, min=1)) -> List[Ingreso]:
+    db = SessionLocal()
+    query = db.query(IngresoModel)
+    result = query.all()
+    return JSONResponse(content=jsonable_encoder(result), status_code=status.HTTP_200_OK)
 
-@ingreso_router.get('/', tags=['ingresos'], response_model=List[Ingreso], description="Returns all ingresos stored")
-def get_all_ingresos() -> List[Ingreso]:
-    result = []
-    for element in ingresos:
-        result.append(element)
-    return JSONResponse(content=result, status_code=200)
 
-@ingreso_router.get('/{id}', tags=['ingresos'], response_model=Ingreso, description="Returns data of one specific ingreso")
-def get_ingreso(id: int) -> Ingreso:
-    for element in ingresos:
-        if element["id"] == id:
-            return JSONResponse(content=element, status_code=200)
-    return JSONResponse(content=None, status_code=404)
+@ingreso_router.get('/{id}',
+                    tags=['ingresos'],
+                    response_model=Ingreso,
+                    description="Returns data of one specific ingreso")
+def get_ingreso(id: int = Path(ge=1, le=5000)) -> Ingreso:
+    db = SessionLocal()
+    element = db.query(IngresoModel).filter(IngresoModel.id == id).first()
+    
+    if not element:
+        return JSONResponse(content={
+            "message": "The requested ingreso was not found",
+            "data": None
+        }, status_code=status.HTTP_404_NOT_FOUND)
+        
+    return JSONResponse(content=jsonable_encoder(element),
+                        status_code=status.HTTP_200_OK)
 
-@ingreso_router.post('/', tags=['ingresos'], response_model=dict, description="Creates a new ingreso")
+
+@ingreso_router.post('/',
+                     tags=['ingresos'],
+                     response_model=dict,
+                     description="Creates a new ingreso")
 def create_ingreso(ingreso: Ingreso = Body()) -> dict:
-    global contador
-    ingreso.id = contador
-    ingreso.fecha = str(datetime.now())
-    contador += 1
-    ingresos.append(ingreso.model_dump())
+    db = SessionLocal()
+    new_ingreso = IngresoModel(**ingreso.model_dump())
+    db.add(new_ingreso)
+    db.commit()
+    
     return JSONResponse(content={
-        "message": "The ingreso was created successfully",
+        "message": "The ingreso was successfully created",
         "data": ingreso.model_dump()
-    }, status_code=201)
+    }, status_code=status.HTTP_201_CREATED)
 
 @ingreso_router.delete('/{id}', tags=['ingresos'], response_model=dict, description="Removes specific ingreso")
 def remove_user(id: int) -> dict:
