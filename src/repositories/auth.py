5239adf6053f5ad56fc8e1e5ba682e23b6ee7175
Victor Manuel <victorakiladev@gmail.com>
2024-05-11 19:09:39 -0500

    fix

--- src/repositories/auth.py
+++ src/repositories/auth.py
@@ -13,7 +13,7 @@ class AuthRepository:
 
 def register_user(self, user: UserCreateSchema) -> dict:
     db = SessionLocal()
-    if UserRepository(db).get_user(email=user.email) != None:
+    if UserRepository(db).get_user(email=user.email) is not None:
         raise Exception("Account already exists")
     hashed_password = auth_handler.hash_password(password=user.password)
     new_user: UserCreateSchema = UserCreateSchema(
